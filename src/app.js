cde67dd704e03ff2d8d52ec8d34a76e27b0b4fec
alkesh <alkesh9639@gmail.com>
2024-04-15 11:44:43 +0530

    add multer and write controllers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import { express } from "express";
+import express from "express"
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 
-const app = Express();
+const app = express();
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
@@ -13,5 +13,15 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true , limit: ' 16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import 
+
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
+
 
 export {app}
