394f26f5ee44e24b3e0dfe582288d673bd22d2bd
alkesh <alkesh9639@gmail.com>
2024-04-23 01:10:07 +0530

    add subscription model and use pipline in user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { 
+    changeCurrentPassword,
+    getCurrentUser, 
+    getUserChannelProfile,
+    getWatchHistory,
+    incomingRefreshToken,
+    loginUser, logoutUser,
+    registerUser, 
+    updateAccountDetails,
+    updateUserAvatar, 
+    updateUserCoverImage 
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleare.js";
 
@@ -24,5 +36,14 @@ router.route("/login").post( loginUser)
 // secure routes 
 
 router.route("/logout").post( verifyJWT ,logoutUser)
+router.route("/refresh-Token").post(incomingRefreshToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-User").get(verifyJWT , getCurrentUser)
+router.route("/update-account").patch(verifyJWT , updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-coverImage").patch(verifyJWT , upload.single("coverImage"), updateUserCoverImage)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
